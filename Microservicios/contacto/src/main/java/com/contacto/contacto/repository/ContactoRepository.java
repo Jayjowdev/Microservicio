dab6cdae46dcb01bd335485ba4575251313a0632
Jayjowdev <ju.barber@duocuc.cl>
2025-11-26 04:25:28 -0300

    Delete Microservicios directory

--- Microservicios/contacto/src/main/java/com/contacto/contacto/repository/ContactoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.contacto.contacto.repository;
-
-import com.contacto.contacto.model.Contacto;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContactoRepository extends CrudRepository<Contacto, Long> {
-}
--- Microservicios/publicacion/src/main/java/com/microservicio/publicacion/repository/PublicacionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.microservicio.publicacion.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.microservicio.publicacion.model.Publicacion;
-
-public interface PublicacionRepository extends JpaRepository<Publicacion, Long> {
-
-}
